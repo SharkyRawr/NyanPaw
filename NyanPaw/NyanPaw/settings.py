cc4f6c12d16acd794fbe2497a865e9c352f10e93
Hendrik 'Bloody' Schumann <bloody@fluffl.es>
2016-07-06 16:28:33 +0200

    Chunked importblockchain.py

--- NyanPaw/NyanPaw/settings.py
+++ NyanPaw/NyanPaw/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': r'H:\\db.sqlite3',
     },
 }
 
